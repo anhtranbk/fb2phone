2f9e5f9a84bcbb28fd22f0c7da173a93da8f2d6b
tjeubaoit <anhtn.bk@gmail.com>
2021-03-05 23:51:24 +0700

    Add user model

---
+++ mainapp/models/user.py
@@ -0,0 +1,51 @@
+from typing import List, Optional
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def create_user(self, email: str, password: Optional[str], first_name: str, **extra_fields) -> "User":
+        """Create and save a User with the given email and password."""
+        if email is None:
+            raise ValueError("Email must be provided!")
+        email = self.normalize_email(email)
+        # extra_fields.setdefault("distinct_id", generate_random_token())
+        user = self.model(email=email, first_name=first_name, **extra_fields)
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS: List[str] = []
+
+    DISABLED = "disabled"
+    TOOLBAR = "toolbar"
+    TOOLBAR_CHOICES = [
+        (DISABLED, DISABLED),
+        (TOOLBAR, TOOLBAR),
+    ]
+
+    username = None  # type: ignore
+    current_organization = models.ForeignKey(
+        "posthog.Organization", models.SET_NULL, null=True, related_name="users_currently+",
+    )
+    current_team = models.ForeignKey("posthog.Team", models.SET_NULL, null=True, related_name="teams_currently+")
+    email = models.EmailField(_("email address"), unique=True)
+    temporary_token: models.CharField = models.CharField(max_length=200, null=True, blank=True, unique=True)
+    distinct_id: models.CharField = models.CharField(max_length=200, null=True, blank=True, unique=True)
+    email_opt_in: models.BooleanField = models.BooleanField(default=False, null=True, blank=True)
+    anonymize_data: models.BooleanField = models.BooleanField(default=False, null=True, blank=True)
+    toolbar_mode: models.CharField = models.CharField(
+        max_length=200, null=True, blank=True, choices=TOOLBAR_CHOICES, default=TOOLBAR
+    )
+
+    objects: UserManager = UserManager()  # type: ignore
